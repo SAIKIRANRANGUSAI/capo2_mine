115c240434a7a5f6acab27056013e4d39a3792e8
SAIKIRANRANGUSAI <saikiranrangu6@gmail.com>
2025-12-05 14:03:08 +0530

    done

--- src/components/otp/Otp.tsx
+++ src/components/otp/Otp.tsx
@@ -127,7 +127,7 @@ const OtpArea = () => {
                 {otp.map((digit, index) => (
                   <input
                     key={index}
-                    ref={(el) => (inputRefs.current[index] = el)}
+                    ref={(el) => { inputRefs.current[index] = el; }}
                     type="text"
                     inputMode="numeric"
                     maxLength={1}
